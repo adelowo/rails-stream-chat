d1e805243d44de5374d1f19413beddeedb96d7a6
Lanre Adelowo <yo@lanre.wtf>
2020-04-21 17:14:15 +0100

    generate token for use by client

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'stream-chat'
+
 class UsersController < ApplicationController
   def create
     # Find user by moniker
@@ -14,7 +16,7 @@ class UsersController < ApplicationController
       user = User.create(user_params)
       if user.valid?
         user.save
-        render json: { status: true, user: user }
+        render json: { status: true, user: user, token: chat_token(user.moniker) }
         return
       end
 
@@ -24,9 +26,24 @@ class UsersController < ApplicationController
     unless user.authenticate(user_params[:password])
       render json: { status: false, message: 'Invalid password provided' }
     end
+
+    render json: { status: true, user: user, token: chat_token(user.moniker) }
   end
 
   private
+
+  def chat_token(username)
+    client = StreamChat::Client.new(api_key = Rails.configuration.stream_api_key, api_secret = Rails.configuration.stream_api_secret)
+    token = client.create_token(username)
+    client.update_user({ id: username, name: username })
+
+    chan = client.channel('messaging', channel_id: 'bob-and-jane')
+    chan.create('admin')
+    chan.add_members(['admin', username])
+    token
+  rescue StandardError => e
+    p e
+  end
 
   def user_params
     params.require(:user).permit(:moniker, :password)
